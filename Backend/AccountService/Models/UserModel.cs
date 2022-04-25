b056a767ca2cb37fb62668a1d23cd29a5935808b
Gaastra486 <427844@student.fontys.nl>
2022-04-25 12:43:34 +0200

    added Account microservice

---
+++ Backend/AccountService/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿namespace AccountService.Models
+{
+    public class UserModel
+    {
+        public string username { get; set; }
+
+        public string password { get; set; }
+        
+        public string email { get; set; }
+
+        public string postalcode { get; set; }  
+
+        public int Id { get; }
+
+
+    }
+}

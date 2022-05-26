2993458f825a924eb2b5ed85c6e929695a7d8726
Gaastra486 <427844@student.fontys.nl>
2022-05-26 11:36:09 +0200

    Added rabbitmq

---
+++ Backend/ClassLibrary/RabbitMQSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace SharedLibrary
+{
+    public class RabbitMQSettings
+    {
+        // RabbitMQ properties;
+
+        public readonly string Username = "guest";
+        public readonly string Password = "guest";
+        // You can put in localhost if you decide to run it locally, otherwise put down the IP address of the server which you're trying to connect to;
+        public readonly string IPAddress = "localhost:5672";
+        public readonly string QueueName = "bid-queue";
+    }
+}
